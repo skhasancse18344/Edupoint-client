86c47b13f4bc8513251b5a633728e68590243e7f
skhasancse18344 <kamrul.hasan.cse18344@gmail.com>
2022-10-24 21:29:05 +0600

    first commit

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,29 @@
+// Import the functions you need from the SDKs you need
+
+import { initializeApp } from "firebase/app";
+
+// TODO: Add SDKs for Firebase products that you want to use
+
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+
+const firebaseConfig = {
+  apiKey: "AIzaSyABgsqk0d7-gm0w5cDdzugHxxdjSgAAywQ",
+
+  authDomain: "edupoint-ab6ad.firebaseapp.com",
+
+  projectId: "edupoint-ab6ad",
+
+  storageBucket: "edupoint-ab6ad.appspot.com",
+
+  messagingSenderId: "384860053389",
+
+  appId: "1:384860053389:web:b6196982bfc4ad00b467c4",
+};
+
+// Initialize Firebase
+
+const app = initializeApp(firebaseConfig);
+
+export default app;
