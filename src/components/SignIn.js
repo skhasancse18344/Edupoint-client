987a9e60675a4949c48d403db587d7ae5c272efb
skhasancse18344 <kamrul.hasan.cse18344@gmail.com>
2022-10-26 02:47:41 +0600

    Login and Logout Page Design

--- src/components/SignIn.js
+++ src/components/SignIn.js
@@ -1,9 +1,40 @@
 import React from "react";
+import Button from "react-bootstrap/Button";
+import Form from "react-bootstrap/Form";
+import { Link } from "react-router-dom";
 
 const SignIn = () => {
   return (
     <div>
-      <h1>This is Sign in Page</h1>
+      <Form className="my-5">
+        <Form.Group className="mb-3" controlId="formBasicEmail">
+          <Form.Label>Email address</Form.Label>
+          <Form.Control type="email" placeholder="Enter email" />
+          <Form.Text className="text-muted">
+            We'll never share your email with anyone else.
+          </Form.Text>
+        </Form.Group>
+
+        <Form.Group className="mb-3" controlId="formBasicPassword">
+          <Form.Label>Password</Form.Label>
+          <Form.Control type="password" placeholder="Password" />
+        </Form.Group>
+        <Form.Group className="mb-3" controlId="formBasicCheckbox">
+          <Form.Check type="checkbox" label="Check me out" />
+        </Form.Group>
+        <Button variant="primary" type="submit">
+          Submit
+        </Button>
+        <p className="mt-3">
+          If You Have No Account Please <Link to={"/SignUp"}>Register</Link>
+        </p>
+      </Form>
+      <div className="text-center my-5">
+        <Button variant="outline-primary">Sign With Google</Button>
+        <Button className="ms-3" variant="outline-primary">
+          Sign With GitHub
+        </Button>
+      </div>
     </div>
   );
 };
--- src/components/SignUp.js
+++ src/components/SignUp.js
@@ -1,9 +1,39 @@
 import React from "react";
+import Button from "react-bootstrap/Button";
+import Form from "react-bootstrap/Form";
+import { Link } from "react-router-dom";
 
 const SignUp = () => {
   return (
     <div>
-      <h1>This is SignUp Page</h1>
+      <Form className="my-5">
+        <Form.Group className="mb-3">
+          <Form.Label>Full Name</Form.Label>
+          <Form.Control type="name" placeholder="Your Full Name" />
+        </Form.Group>
+        <Form.Group className="mb-3">
+          <Form.Label>Photo URL</Form.Label>
+          <Form.Control type="photoURL" placeholder="Your Photo URL" />
+        </Form.Group>
+        <Form.Group className="mb-3" controlId="formBasicEmail">
+          <Form.Label>Email address</Form.Label>
+          <Form.Control type="email" placeholder="Enter email" />
+          <Form.Text className="text-muted">
+            We'll never share your email with anyone else.
+          </Form.Text>
+        </Form.Group>
+
+        <Form.Group className="mb-3" controlId="formBasicPassword">
+          <Form.Label>Password</Form.Label>
+          <Form.Control type="password" placeholder="Password" />
+        </Form.Group>
+        <Button variant="primary" type="submit">
+          Submit
+        </Button>
+      </Form>
+      <p className="my-5">
+        If You Have Already An Account Please <Link to={"/SignIn"}>Log in</Link>
+      </p>
     </div>
   );
 };
