48064f1e0791fa3def0fa4e7d75a54e5f6433a18
skhasancse18344 <kamrul.hasan.cse18344@gmail.com>
2022-10-27 16:55:01 +0600

    Pdf Button

--- src/components/CourseDetail.js
+++ src/components/CourseDetail.js
@@ -2,13 +2,29 @@ import React from "react";
 import { Link, useLoaderData } from "react-router-dom";
 import Button from "react-bootstrap/Button";
 import Card from "react-bootstrap/Card";
+import Pdf from "react-to-pdf";
+
+import { FaRegFilePdf } from "react-icons/fa";
+const ref = React.createRef();
 
 const CourseDetail = () => {
   const details = useLoaderData();
   return (
-    <div>
-      <Card className="text-center">
+    <div className="my-5">
+      <Card className="text-center w-50 mx-auto" ref={ref}>
         <Card.Body>
+          <div>
+            <Pdf targetRef={ref} filename="code-example.pdf">
+              {({ toPdf }) => (
+                <button
+                  className="my-5 fs-2 rounded text-info border-info"
+                  onClick={toPdf}
+                >
+                  <FaRegFilePdf></FaRegFilePdf>
+                </button>
+              )}
+            </Pdf>
+          </div>
           <img className="img-fluid" src={details.image} alt="" />
           <Card.Title className="mt-5">{details.name}</Card.Title>
           <Card.Text className="w-75 m-auto mt-5 mb-5">
